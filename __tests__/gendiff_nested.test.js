39894bd7f7a1a9d879dd36806e11a37cb285b40d
Andrey Abramov <abramow-a.v@ya.ru>
2024-01-29 23:00:39 +0300

    try add recurse

---
+++ __tests__/gendiff_nested.test.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import gendiff from '../src/genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('nested_json_files', () => {
+  expect(gendiff(
+    './__fixtures__/nested_structures/file1.json',
+    './__fixtures__/nested_structures/file2.json',
+  )).toBe(readFile('/nested_structures/nested_json_result.txt'));
+});
