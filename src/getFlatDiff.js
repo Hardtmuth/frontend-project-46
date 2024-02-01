8b2280f11b0ea71a81e4116a8844bac5688a6427
Andrey Abramov <abramow-a.v@ya.ru>
2024-02-01 22:15:32 +0300

    rewrite flat diff with stylish

--- src/getFlatDiff.js
+++ src/getFlatDiff.js
@@ -12,17 +12,46 @@ const someData2 = {
 const getDiff = (data1, data2) => {
   const keys = Object.keys({ ...data1, ...data2 });
 
-  const prepare = keys.map((item) => {
-    const result = [0, item];
-    const val1 = data1 === undefined ? undefined : data1[item];
-    const val2 = data2 === undefined ? undefined : data2[item];
+  const prepare = keys.map((key) => {
+    let prefix = ' ';
+    const val1 = data1 === undefined ? undefined : data1[key];
+    const val2 = data2 === undefined ? undefined : data2[key];
 
-    if (val1 !== val2) {
-      result[0] = 1;
+    if (val1 === undefined && val2 !== undefined) {
+      prefix = '+';
+    } else if (val1 !== undefined && val2 === undefined) {
+      prefix = '-';
+    } else if (val1 !== val2) {
+      prefix = '±';
     }
-    return [...result, val1, val2];
+
+    return [prefix, key, val1, val2];
   });
-  return prepare;
+
+  const stylish = (preparingData, spaceSymbol = ' ', spaceCount = 2) => {
+    const space = spaceSymbol.repeat(spaceCount);
+    const result = preparingData.map((item) => {
+      const [prefix, key, value1, value2] = item;
+      switch (prefix) {
+        case ' ':
+          return `${space}${prefix} ${key}: ${value1}\n`;
+        case '+':
+          return `${space}${prefix} ${key}: ${value2}\n`;
+        case '-':
+          return `${space}${prefix} ${key}: ${value1}\n`;
+        case '±':
+          return `${space}- ${key}: ${value1}\n`
+               + `${space}+ ${key}: ${value2}\n`;
+        default:
+          return null;
+      }
+    });
+    console.log(result);
+
+    return `{\n${result.join('')}}`;
+  };
+
+  return stylish(prepare);
 };
 
 console.log(getDiff(someData1, someData2));
