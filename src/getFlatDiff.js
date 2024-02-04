018d6061cb0bfb24cf08d4014a7b36d1c27e84d9
Andrey Abramov <abramow-a.v@ya.ru>
2024-02-05 00:27:34 +0300

    Revert "another refactoring for codeclimate"
    This reverts commit c779867181df241fb7fcf9ef8e4bcbee634a0293.

--- src/getFlatDiff.js
+++ src/getFlatDiff.js
@@ -6,33 +6,39 @@ const isFlat = (data) => (typeof data !== 'object' || data === null);
 const getDiff = (data1, data2, deep = 1) => {
   const keys = _.sortBy(Object.keys({ ...data1, ...data2 }));
 
-  const getPrepareData = (key) => {
+  const cb = (key) => {
     let prefix = ' ';
-    let v1 = data1 === undefined ? undefined : data1[key];
-    let v2 = data2 === undefined ? undefined : data2[key];
+    let val1 = data1 === undefined ? undefined : data1[key];
+    let val2 = data2 === undefined ? undefined : data2[key];
 
-    const prepv1 = isFlat(v1) ? v1 : stylish(getDiff(v1, v1, deep + 1), deep + 1);
-    const prepv2 = isFlat(v2) ? v2 : stylish(getDiff(v2, v2, deep + 1), deep + 1);
+    const someFlat = (
+      (isFlat(val1) && isFlat(val2))
+      || (!isFlat(val1) && isFlat(val2))
+      || (isFlat(val1) && !isFlat(val2))
+    );
 
-    if ((isFlat(v1) && isFlat(v2)) || (!isFlat(v1) && isFlat(v2)) || (isFlat(v1) && !isFlat(v2))) {
-      v1 = prepv1;
-      v2 = prepv2;
-      if (v1 === undefined && v2 !== undefined) {
+    const prepVal1 = isFlat(val1) ? val1 : stylish(getDiff(val1, val1, deep + 1), deep + 1);
+    const prepVal2 = isFlat(val2) ? val2 : stylish(getDiff(val2, val2, deep + 1), deep + 1);
+
+    if (someFlat) {
+      val1 = prepVal1;
+      val2 = prepVal2;
+      if (val1 === undefined && val2 !== undefined) {
         prefix = '+';
-      } else if (v1 !== undefined && v2 === undefined) {
+      } else if (val1 !== undefined && val2 === undefined) {
         prefix = '-';
-      } else if (v1 !== v2) {
+      } else if (val1 !== val2) {
         prefix = '±';
       }
     } else {
-      v1 = stylish(getDiff(v1, v2, deep + 1), deep + 1);
-      v2 = v1;
+      val1 = stylish(getDiff(val1, val2, deep + 1), deep + 1);
+      val2 = val1;
     }
 
-    return [prefix, key, v1, v2, deep];
+    return [prefix, key, val1, val2, deep];
   };
 
-  return keys.map(getPrepareData);
+  return keys.map(cb);
 };
 
 export default getDiff;
