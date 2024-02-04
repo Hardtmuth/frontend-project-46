e0019bacf559df2ccea3b15cf231723e95b1d972
Andrey Abramov <abramow-a.v@ya.ru>
2024-02-04 23:36:19 +0300

    fix get diff nested files

--- src/getFlatDiff.js
+++ src/getFlatDiff.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import stylish from './stylish.js';
 
-const isFlat = (data) => (typeof data !== 'object');// && data !== null);
+const isFlat = (data) => (typeof data !== 'object' || data === null);
 
 const getDiff = (data1, data2, deep = 1) => {
   const keys = _.sortBy(Object.keys({ ...data1, ...data2 }));
@@ -11,13 +11,25 @@ const getDiff = (data1, data2, deep = 1) => {
     let val1 = data1 === undefined ? undefined : data1[key];
     let val2 = data2 === undefined ? undefined : data2[key];
 
-    if (isFlat(val1) && isFlat(val2)) {
+    const someFlat = (
+      (isFlat(val1) && isFlat(val2))
+      || (!isFlat(val1) && isFlat(val2))
+      || (isFlat(val1) && !isFlat(val2))
+    );
+
+    if (someFlat) {
       if (val1 === undefined && val2 !== undefined) {
         prefix = '+';
+        val1 = isFlat(val1) ? val1 : stylish(getDiff(val1, val1, deep + 1), deep + 1);
+        val2 = isFlat(val2) ? val2 : stylish(getDiff(val2, val2, deep + 1), deep + 1);
       } else if (val1 !== undefined && val2 === undefined) {
         prefix = '-';
+        val1 = isFlat(val1) ? val1 : stylish(getDiff(val1, val1, deep + 1), deep + 1);
+        val2 = isFlat(val2) ? val2 : stylish(getDiff(val2, val2, deep + 1), deep + 1);
       } else if (val1 !== val2) {
         prefix = '±';
+        val1 = isFlat(val1) ? val1 : stylish(getDiff(val1, val1, deep + 1), deep + 1);
+        val2 = isFlat(val2) ? val2 : stylish(getDiff(val2, val2, deep + 1), deep + 1);
       }
     } else {
       // prefix = ' ';
