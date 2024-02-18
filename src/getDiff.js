16b6a4bb987457ff455be3eeea6890790e5ec357
Andrey Abramov <abramow-a.v@ya.ru>
2024-02-18 18:26:30 +0300

    change getDiff to immutable?

--- src/getDiff.js
+++ src/getDiff.js
@@ -3,35 +3,49 @@ import _ from 'lodash';
 export const isFlat = (data) => typeof data !== 'object' || data === null;
 
 const getDiff = (data1, data2, depth = 1) => {
-  const keys = _.sortBy(Object.keys({ ...data1, ...data2 }));
+  const keys = _.sortBy(_.keys(_.assign({}, data1, data2)));
 
-  const result = keys.map((key) => {
-    const res = { key };
+  const result = _.map(keys, (key) => {
     const stringifiedData1 = JSON.stringify(data1[key]);
     const stringifiedData2 = JSON.stringify(data2[key]);
 
     if (stringifiedData1 && !stringifiedData2) {
-      res.mod = 'removed';
-      res.value = data1[key];
-    } else if (!stringifiedData1 && stringifiedData2) {
-      res.mod = 'added';
-      res.value = data2[key];
-    } else if (stringifiedData1 !== stringifiedData2) {
-      if (!isFlat(data1[key]) && !isFlat(data2[key])) {
-        res.mod = 'nested_change';
-        res.value = getDiff(data1[key], data2[key], depth + 1);
-        res.depth = depth;
-      } else {
-        res.mod = 'updated';
-        res.old_value = data1[key];
-        res.new_value = data2[key];
-      }
-    } else {
-      res.mod = 'not_modify';
-      res.value = data1[key];
+      return {
+        key,
+        mod: 'removed',
+        value: data1[key],
+      };
     }
-    return res;
+    if (!stringifiedData1 && stringifiedData2) {
+      return {
+        key,
+        mod: 'added',
+        value: data2[key],
+      };
+    }
+    if (stringifiedData1 !== stringifiedData2) {
+      if (!isFlat(data1[key]) && !isFlat(data2[key])) {
+        return {
+          key,
+          mod: 'nested_change',
+          value: getDiff(data1[key], data2[key], depth + 1),
+          depth,
+        };
+      }
+      return {
+        key,
+        mod: 'updated',
+        old_value: data1[key],
+        new_value: data2[key],
+      };
+    }
+    return {
+      key,
+      mod: 'not_modify',
+      value: data1[key],
+    };
   });
   return result;
 };
+
 export default getDiff;
