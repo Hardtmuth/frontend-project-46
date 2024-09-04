a2c46a0645b8d24c2357d151fcea7761fe04d635
Andrey Abramov <abramow-a.v@ya.ru>
2024-09-04 20:59:41 +0300

    changes for codeclimate

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -15,18 +15,24 @@ const stylish = (preparingData, depth = 1, indentSymbol = ' ', indentCount = 4) 
   const getStylishString = (acc, item) => {
     const deep = item?.depth || depth;
     const indent = indentSymbol.repeat(indentCount * deep - 2);
+    let resultString = '';
+    let val = '';
     if (['added', 'removed', 'not_modify'].includes(item.mod)) {
       const diff = { added: '+', removed: '-', not_modify: ' ' };
-      return `${acc}${indent}${diff[item.mod]} ${item.key}: ${stringify(item.value, depth + 1)}\n`;
+      val = stringify(item.value, depth + 1);
+      resultString = `${acc}${indent}${diff[item.mod]} ${item.key}: ${val}\n`;
     }
     if (item.mod === 'nested_change') {
-      return `${acc}${indent}  ${item.key}: ${stylish(item.value, depth + 1)}\n`;
+      val = stylish(item.value, depth + 1);
+      resultString = `${acc}${indent}  ${item.key}: ${val}\n`;
     }
     if (item.mod === 'updated') {
-      return `${acc}${indent}- ${item.key}: ${stringify(item.old_value, depth + 1)}\n`
-           + `${indent}+ ${item.key}: ${stringify(item.new_value, depth + 1)}\n`;
+      val = stringify(item.old_value, depth + 1);
+      const val2 = stringify(item.new_value, depth + 1);
+      resultString = `${acc}${indent}- ${item.key}: ${val}\n`
+                   + `${indent}+ ${item.key}: ${val2}\n`;
     }
-    return null;
+    return resultString;
   };
   const lastIndent = indentSymbol.repeat(indentCount * depth - indentCount);
   return `{\n${preparingData.reduce(getStylishString, '')}${lastIndent}}`;
--- src/getDiff.js
+++ src/getDiff.js
@@ -9,41 +9,31 @@ const getDiff = (data1, data2, depth = 1) => {
     const stringifiedData1 = JSON.stringify(data1[key]);
     const stringifiedData2 = JSON.stringify(data2[key]);
 
-    if (stringifiedData1 && !stringifiedData2) {
-      return {
-        key,
-        mod: 'removed',
-        value: data1[key],
-      };
-    }
-    if (!stringifiedData1 && stringifiedData2) {
-      return {
-        key,
-        mod: 'added',
-        value: data2[key],
-      };
-    }
-    if (stringifiedData1 !== stringifiedData2) {
-      if (!isFlat(data1[key]) && !isFlat(data2[key])) {
-        return {
-          key,
-          mod: 'nested_change',
-          value: getDiff(data1[key], data2[key], depth + 1),
-          depth,
-        };
-      }
-      return {
-        key,
-        mod: 'updated',
-        old_value: data1[key],
-        new_value: data2[key],
-      };
-    }
-    return {
+    const obj = {
       key,
-      mod: 'not_modify',
-      value: data1[key],
     };
+
+    if (stringifiedData1 && !stringifiedData2) {
+      obj.mod = 'removed';
+      obj.value = data1[key];
+    } else if (!stringifiedData1 && stringifiedData2) {
+      obj.mod = 'added';
+      obj.value = data2[key];
+    } else if (stringifiedData1 !== stringifiedData2) {
+      if (!isFlat(data1[key]) && !isFlat(data2[key])) {
+        obj.mod = 'nested_change';
+        obj.value = getDiff(data1[key], data2[key], depth + 1);
+        obj.depth = parseInt(depth, 10);
+      } else {
+        obj.mod = 'updated';
+        obj.old_value = data1[key];
+        obj.new_value = data2[key];
+      }
+    } else {
+      obj.mod = 'not_modify';
+      obj.value = data1[key];
+    }
+    return obj;
   });
   return result;
 };
