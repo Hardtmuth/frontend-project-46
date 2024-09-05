35b387b5bb125eba07ed1f227c7d189c3f42928b
Andrey Abramov <abramow-a.v@ya.ru>
2024-09-05 08:38:08 +0300

    code complexity check

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -15,10 +15,7 @@ const stylish = (preparingData, depth = 1, indentSymbol = ' ', indentCount = 4) 
   const getStylishString = (acc, item) => {
     const deep = item?.depth || depth;
     const indent = indentSymbol.repeat(indentCount * deep - 2);
-    if (['added', 'removed', 'not_modify'].includes(item.mod)) {
-      const diff = { added: '+', removed: '-', not_modify: ' ' };
-      return `${acc}${indent}${diff[item.mod]} ${item.key}: ${stringify(item.value, depth + 1)}\n`;
-    }
+    const diff = { added: '+', removed: '-', not_modify: ' ' };
     if (item.mod === 'nested_change') {
       return `${acc}${indent}  ${item.key}: ${stylish(item.value, depth + 1)}\n`;
     }
@@ -26,7 +23,7 @@ const stylish = (preparingData, depth = 1, indentSymbol = ' ', indentCount = 4) 
       return `${acc}${indent}- ${item.key}: ${stringify(item.value, depth + 1)}\n`
            + `${indent}+ ${item.key}: ${stringify(item.new_value, depth + 1)}\n`;
     }
-    return null;
+    return `${acc}${indent}${diff[item.mod]} ${item.key}: ${stringify(item.value, depth + 1)}\n`;
   };
   const lastIndent = indentSymbol.repeat(indentCount * depth - indentCount);
   return `{\n${preparingData.reduce(getStylishString, '')}${lastIndent}}`;
--- src/getDiff.js
+++ src/getDiff.js
@@ -1,37 +1,48 @@
 import _ from 'lodash';
 
+const comparingData = (dataSet1, dataSet2) => {
+  if (dataSet1 && !dataSet2) {
+    return 'stageOne';
+  }
+  if (!dataSet1 && dataSet2) {
+    return 'stageTwo';
+  }
+  if (dataSet1 !== dataSet2) {
+    return 'notEqual';
+  }
+  return 'equal';
+};
+
 const getDiff = (data1, data2, depth = 1) => {
   const keys = _.sortBy(_.keys(_.assign({}, data1, data2)));
 
-  const result = _.map(keys, (key) => {
-    const stringifiedData1 = JSON.stringify(data1[key]);
-    const stringifiedData2 = JSON.stringify(data2[key]);
+  return _.map(keys, (key) => {
+    const stage = comparingData(JSON.stringify(data1[key]), JSON.stringify(data2[key]));
 
-    if (stringifiedData1 && !stringifiedData2) {
-      return { key, mod: 'removed', value: data1[key] };
-    }
-    if (!stringifiedData1 && stringifiedData2) {
-      return { key, mod: 'added', value: data2[key] };
-    }
-    if (stringifiedData1 !== stringifiedData2) {
-      if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+    switch (stage) {
+      case 'stageOne':
+        return { key, mod: 'removed', value: data1[key] };
+      case 'stageTwo':
+        return { key, mod: 'added', value: data2[key] };
+      case 'notEqual':
+        if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+          return {
+            key,
+            mod: 'nested_change',
+            value: getDiff(data1[key], data2[key], depth + 1),
+            depth,
+          };
+        }
         return {
           key,
-          mod: 'nested_change',
-          value: getDiff(data1[key], data2[key], depth + 1),
-          depth,
+          mod: 'updated',
+          value: data1[key],
+          new_value: data2[key],
         };
-      }
-      return {
-        key,
-        mod: 'updated',
-        value: data1[key],
-        new_value: data2[key],
-      };
+      default:
+        return { key, mod: 'not_modify', value: data1[key] };
     }
-    return { key, mod: 'not_modify', value: data1[key] };
   });
-  return result;
 };
 
 export default getDiff;
